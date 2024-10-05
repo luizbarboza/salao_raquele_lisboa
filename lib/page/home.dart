e675eb5d31d6bffad0ccfbf5fd345082c63caf51
Luiz Felipe Machado Barboza <32441588+luizbarboza@users.noreply.github.com>
2024-10-05 12:53:36 -0700

    constructors for public widgets should have a named 'key' parameter.

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -4,6 +4,8 @@ import 'agendamento.dart';
 import 'profile.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   HomePageState createState() => HomePageState();
 }
