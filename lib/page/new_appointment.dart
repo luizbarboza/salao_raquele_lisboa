7397a727e3f2b4d97656c0081786a7b3bf1f3887
asapdeki <mandaparaogustavo@gmail.com>
2024-10-09 22:27:07 -0300

    new_appointment alterado

--- lib/page/new_appointment.dart
+++ lib/page/new_appointment.dart
@@ -13,6 +13,39 @@ import '../bloc/specialist_state.dart';
 import '../bloc/specialty.dart';
 import '../bloc/specialty_state.dart';
 
+void main() {
+  runApp(const SalaRaqueleLisboaApp());
+}
+
+class SalaRaqueleLisboaApp extends StatelessWidget {
+  const SalaRaqueleLisboaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sala Raquele Lisboa',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+      home: const NewAppointmentPage(),
+    );
+  }
+}
+
 class NewAppointmentPage extends StatefulWidget {
   const NewAppointmentPage({super.key});
 
@@ -21,17 +54,10 @@ class NewAppointmentPage extends StatefulWidget {
 }
 
 class NewAppointmentPageState extends State<NewAppointmentPage> {
-  //TextEditingController _specialtyController = TextEditingController();
   int? _selectedSpecialty;
-  //TextEditingController _specialistController = TextEditingController();
   int? _selectedSpecialist;
   DateTime? _pickedDateTime;
   TextEditingController _dateTimeController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   Future<void> _selectDate(BuildContext context) async {
     DateTime? pickedDate = await showDatePicker(
@@ -42,14 +68,12 @@ class NewAppointmentPageState extends State<NewAppointmentPage> {
     );
 
     if (pickedDate != null) {
-      // Seleciona a hora
       TimeOfDay? pickedTime = await showTimePicker(
         context: context,
         initialTime: TimeOfDay.now(),
       );
 
       if (pickedTime != null) {
-        // Combina data e hora
         DateTime fullDateTime = DateTime(
           pickedDate.year,
           pickedDate.month,
@@ -59,7 +83,6 @@ class NewAppointmentPageState extends State<NewAppointmentPage> {
         );
 
         setState(() {
-          // Atualiza o controlador com a data e hora selecionada
           _pickedDateTime = fullDateTime;
           _dateTimeController.text =
               "${fullDateTime.day}/${fullDateTime.month}/${fullDateTime.year} ${pickedTime.format(context)}";
@@ -93,103 +116,114 @@ class NewAppointmentPageState extends State<NewAppointmentPage> {
           }
         },
         child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Novo Agendamento'),
+          ),
           body: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  BlocBuilder<SpecialtyBloc, SpecialtyState>(
-                      builder: (context, state) {
-                    var dropdownMenuEntries = <DropdownMenuEntry<int?>>[];
-                    if (state is SpecialtyFetchedAll) {
-                      dropdownMenuEntries = state.specialties
-                          .map((specialty) => DropdownMenuEntry(
-                                value: specialty.id,
-                                label: specialty.name,
-                              ))
-                          .toList();
-                    }
-                    return DropdownMenu<int?>(
-                      width: 300,
-                      initialSelection: null,
-                      label: const Text('Especialidade'),
-                      onSelected: (selectedSpecialty) {
-                        if (selectedSpecialty != null) {
-                          context.read<SpecialistBloc>().add(SpecialistFetch({
-                                "especialidade": selectedSpecialty,
-                              }));
-                        }
-                        setState(() {
-                          _selectedSpecialty = selectedSpecialty;
-                        });
-                      },
-                      dropdownMenuEntries: dropdownMenuEntries,
-                    );
-                  }),
-                  const SizedBox(height: 20),
-                  BlocBuilder<SpecialistBloc, SpecialistState>(
-                      builder: (context, state) {
-                    var dropdownMenuEntries = <DropdownMenuEntry<int?>>[];
-                    if (_selectedSpecialty != null &&
-                        state is SpecialistFetched) {
-                      dropdownMenuEntries = state.specialist
-                          .map((specialist) => DropdownMenuEntry(
-                                value: specialist.id,
-                                label: specialist.person.name,
-                              ))
-                          .toList();
-                    }
-                    return DropdownMenu<int?>(
-                      width: 300,
-                      initialSelection: null,
-                      label: const Text('Especialista'),
-                      onSelected: (selectedSpecialist) {
-                        setState(() {
-                          _selectedSpecialist = selectedSpecialist;
-                        });
-                      },
-                      dropdownMenuEntries: dropdownMenuEntries,
-                    );
-                  }),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                      controller: _dateTimeController,
-                      decoration: InputDecoration(
-                        labelText: 'Data e hora',
-                        suffixIcon: IconButton(
-                          icon: const Icon(Icons.calendar_today),
-                          onPressed: () => _selectDate(context),
-                        ),
-                        border: const OutlineInputBorder(),
-                      ),
-                      readOnly:
-                          true, // O campo não será editável diretamente, apenas via DatePicker
-                      onTap: () => _selectDate(
-                          context), // Abre o DatePicker ao tocar no campo
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Adicionando a imagem acima das caixas de seleção
+                    Image.network(
+                      'https://png.pngtree.com/png-vector/20190429/ourmid/pngtree-agenda-icon-vector-illustration-in-line-style-for-any-purpose-png-image_996811.jpg',
+                      height: 100, // Ajuste de tamanho da imagem
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
-                    return FilledButton(
-                      onPressed: () {
-                        final auth = context.read<AuthBloc>();
-                        final authState = auth.state;
-                        if (authState is AuthAuthenticated) {
-                          final appointment = context.read<AppointmentBloc>();
-                          appointment.add(AppointmentInsert({
-                            "cliente": authState.person.id,
-                            "especialista": _selectedSpecialist!,
-                            "data_hora": DateFormat('yyyy-MM-dd hh:mm:ss')
-                                .format(_pickedDateTime!),
-                          }));
-                        }
-                      },
-                      child: const Text('Agendar'),
-                    );
-                  }),
-                ],
+                    const SizedBox(height: 20), // Espaço entre imagem e seleções
+                    BlocBuilder<SpecialtyBloc, SpecialtyState>(
+                        builder: (context, state) {
+                      var dropdownMenuEntries = <DropdownMenuEntry<int?>>[];
+                      if (state is SpecialtyFetchedAll) {
+                        dropdownMenuEntries = state.specialties
+                            .map((specialty) => DropdownMenuEntry(
+                                  value: specialty.id,
+                                  label: specialty.name,
+                                ))
+                            .toList();
+                      }
+                      return DropdownMenu<int?>(
+                        width: 300,
+                        initialSelection: null,
+                        label: const Text('Especialidade'),
+                        onSelected: (selectedSpecialty) {
+                          if (selectedSpecialty != null) {
+                            context.read<SpecialistBloc>().add(SpecialistFetch({
+                                  "especialidade": selectedSpecialty,
+                                }));
+                          }
+                          setState(() {
+                            _selectedSpecialty = selectedSpecialty;
+                          });
+                        },
+                        dropdownMenuEntries: dropdownMenuEntries,
+                      );
+                    }),
+                    const SizedBox(height: 20),
+                    BlocBuilder<SpecialistBloc, SpecialistState>(
+                        builder: (context, state) {
+                      var dropdownMenuEntries = <DropdownMenuEntry<int?>>[];
+                      if (_selectedSpecialty != null &&
+                          state is SpecialistFetched) {
+                        dropdownMenuEntries = state.specialist
+                            .map((specialist) => DropdownMenuEntry(
+                                  value: specialist.id,
+                                  label: specialist.person.name,
+                                ))
+                            .toList();
+                      }
+                      return DropdownMenu<int?>(
+                        width: 300,
+                        initialSelection: null,
+                        label: const Text('Especialista'),
+                        onSelected: (selectedSpecialist) {
+                          setState(() {
+                            _selectedSpecialist = selectedSpecialist;
+                          });
+                        },
+                        dropdownMenuEntries: dropdownMenuEntries,
+                      );
+                    }),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        controller: _dateTimeController,
+                        decoration: InputDecoration(
+                          labelText: 'Data e hora',
+                          suffixIcon: IconButton(
+                            icon: const Icon(Icons.calendar_today),
+                            onPressed: () => _selectDate(context),
+                          ),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
+                      return ElevatedButton.icon(
+                        icon: const Icon(Icons.schedule),
+                        onPressed: () {
+                          final auth = context.read<AuthBloc>();
+                          final authState = auth.state;
+                          if (authState is AuthAuthenticated) {
+                            final appointment =
+                                context.read<AppointmentBloc>();
+                            appointment.add(AppointmentInsert({
+                              "cliente": authState.person.id,
+                              "especialista": _selectedSpecialist!,
+                              "data_hora": DateFormat('yyyy-MM-dd hh:mm:ss')
+                                  .format(_pickedDateTime!),
+                            }));
+                          }
+                        },
+                        label: const Text('Agendar'),
+                      );
+                    }),
+                  ],
+                ),
               ),
             ),
           ),
